965915cf9e0c5de892f8dbb03193adf53368a023
Morgan Kurth <mokurth.mk@gmail.com>
2024-10-31 10:22:54 -0600

    Fixing requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name = 'fortnight', # name of the package
     version = '0.1.1',
     packages = 'fortnight', # could also do a list if you have a bunch # or find_packages() will find init.py modules and gets that package
-    requires=['numpy <= 2.0', 'pandas']
+    install_requires=['numpy <= 2.0', 'pandas']
     ) 
